ea73323137ffafd9c065dde3fdbf78b50fac66d0
Anique <aniquedavla@gmail.com>
2016-11-19 22:31:53 -0800

    bottom div size fixed

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,34 +1,45 @@
-// function adjustStyle() {
-//      Height = viewport(height - element.offset.top - desired bottom message)
+//$().onready avoided because it would override onload
+//when loaded
+window.onload = function(){
+        setDivSize();
+}
+//when window is resized.
+window.onresize = function(){
+        setDivSize();
+}
+setDivSize = function(){
+    var element = document.getElementById("main-header");
+    var headerHeight = window.getComputedStyle(element,null).getPropertyValue("height");
+    console.log(headerHeight);
+    heightString = headerHeight.replace(/\D/g,"");
+    heightInt = parseInt(heightString);
+    console.log(heightInt);
+    var heightAvailable = (window.innerHeight) - heightInt;
+    console.log(heightAvailable);
+    var contentDiv = document.getElementById("page-content");
+    $("#page-content").css({
+        "height": heightAvailable
+    });
+}
 
-//     var width = 0;
-//     // get the width.. more cross-browser issues
-//     if (window.innerHeight) {
-//         width = window.innerWidth;
-//     } else if (document.documentElement && document.documentElement.clientHeight) {
-//         width = document.documentElement.clientWidth;
-//     } else if (document.body) {
-//         width = document.body.clientWidth;
-//     }
-//     // now we should have it
-//     if (width < 600) {
-//         document.getElementById("myCSS").setAttribute("href", "_css/narrow.css");
-//     } else {
-//         document.getElementById("myCSS").setAttribute("href", "_css/main.css");
-//     }
-// }
 
-// // when window is resized.
-// window.onresize = function () {
-//     adjustStyle();
-// };
+// // function adjustStyle() {
+// //      Height = viewport(height - element.offset.top - desired bottom message)
 
-// //when loaded/
-// window.onload = function(){
-//     adjustStyle();
-// }
+// //     var width = 0;
+// //     // get the width.. more cross-browser issues
+// //     if (window.innerHeight) {
+// //         width = window.innerWidth;
+// //     } else if (document.documentElement && document.documentElement.clientHeight) {
+// //         width = document.documentElement.clientWidth;
+// //     } else if (document.body) {
+// //         width = document.body.clientWidth;
+// //     }
+// //     // now we should have it
+// //     if (width < 600) {
+// //         document.getElementById("myCSS").setAttribute("href", "_css/narrow.css");
+// //     } else {
+// //         document.getElementById("myCSS").setAttribute("href", "_css/main.css");
+// //     }
+// // }
 //
-$(function(){
-  headerHeight = $(".page-header").outerHeight();
-  $(".page-content").css("height":"50px");
-  });
